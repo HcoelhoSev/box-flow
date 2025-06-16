2b2c4cca3ad7c461e754d3f25ac6b24d0bc18cd5
Henrique <hcoelhosev@hotmail.com>
2025-06-15 23:09:09 -0300

    commit

---
+++ src/main/java/com/flowbox/flowbox/FlowboxApplication.java
@@ -0,0 +1,12 @@
+package com.flowbox.flowbox;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlowboxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlowboxApplication.class, args);
+	}
+}
---
+++ src/test/java/com/flowbox/flowbox/FlowboxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.flowbox.flowbox;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlowboxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
